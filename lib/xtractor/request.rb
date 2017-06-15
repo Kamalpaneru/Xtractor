69daa9f9b7a0c3a462eaa74adc12ccd2820b6f4f
kamalpaneru <kamalpaneru.15@gmail.com>
2017-06-15 17:36:35 +0545

    Integrate Azure CV API

---
+++ lib/xtractor/request.rb
@@ -0,0 +1,26 @@
+require 'net/http'
+
+uri = URI('https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/ocr')
+uri.query = URI.encode_www_form({
+
+    'language' => 'unk',
+    'detectOrientation ' => 'true'
+})
+
+request = Net::HTTP::Post.new(uri.request_uri)
+
+request['Content-Type'] = 'application/octet-stream'
+
+request['Ocp-Apim-Subscription-Key'] = '43f80a0ab4d5441d8e0d292e19e5d3c9'
+
+request.body = File.binread('')
+
+
+response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+    http.request(request)
+end
+
+json_file = File.open('json_data.tsv','w')
+
+json_file.puts (response.body)
+
