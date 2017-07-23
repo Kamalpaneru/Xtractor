2e31fb0e7335eed3695098c449e7ab100ce28a27
kamalpaneru <kamalpaneru.15@gmail.com>
2017-07-23 18:26:16 +0545

    Multiple image request to API

--- lib/xtractor/request.rb
+++ lib/xtractor/request.rb
@@ -1,7 +1,7 @@
 require 'net/http'
 
 def request_API
-  @directory = File.expand_path("cell-files",File.dirname(__FILE__))
+  #@directory = File.expand_path("cell-files",File.dirname(__FILE__))
   uri = URI('https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/ocr')
   uri.query = URI.encode_www_form({
 
@@ -13,22 +13,26 @@ def request_API
 
   request['Content-Type'] = 'application/octet-stream'
 
-  request['Ocp-Apim-Subscription-Key'] = '43f80a0ab4d5441d8e0d292e19e5d3c9'
+  request['Ocp-Apim-Subscription-Key'] = "664a0b22b4014e2d916b9fc6a05b5fe6"
 
 
-  Dir.glob("#{@directory}/*.{jpg.png.gif}") do |crop_image|
-    request.body = File.binread(crop_image)
+
+    json_file = File.open('json_data.tsv','w')
+
+  Dir.glob("cell-files/*.{jpg,png,gif}") do |crop_image|
+    request.body = File.binread("#{crop_image}")
 
     response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
       http.request(request)
     end
 
-    json_file = File.open('json_data.tsv','w')
-
     json_file.puts(response.body)
+
   end
-  json_file.close
+   json_file.close
 end
+
+
 
 
 
