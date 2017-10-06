7a4ef2895eecf773fca9074437be7bdc5a6fb6f2
kamalpaneru <kamalpaneru.15@gmail.com>
2017-10-06 14:53:00 +0545

    Integrate Azure Computer Vision API

--- lib/xtractor/request.rb
+++ lib/xtractor/request.rb
@@ -1,36 +1,52 @@
 require 'net/http'
+require 'json'
 
 def request_API
-  #@directory = File.expand_path("cell-files",File.dirname(__FILE__))
   uri = URI('https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/ocr')
   uri.query = URI.encode_www_form({
 
-    'language' => 'unk',
+    'language' => 'en',
     'detectOrientation ' => 'true'
+
   })
 
   request = Net::HTTP::Post.new(uri.request_uri)
 
   request['Content-Type'] = 'application/octet-stream'
 
-  request['Ocp-Apim-Subscription-Key'] = "664a0b22b4014e2d916b9fc6a05b5fe6"
+  request['Ocp-Apim-Subscription-Key'] = "4ba1e7dfca694ca7bfce73b99988395c"
 
+  collect = Hash.new
 
+  File.open('final.json', "w") do |data|
+    all_files = Dir.glob("cell-files/*.{jpg,png,gif}").sort
 
-    json_file = File.open('json_data.tsv','w')
+    all_files.each do |crop_image|
+     request.body = File.binread("#{crop_image}")
 
-  Dir.glob("cell-files/*.{jpg,png,gif}") do |crop_image|
-    request.body = File.binread("#{crop_image}")
+      response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+       http.request(request)
+      end
 
-    response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
-      http.request(request)
+      collect[crop_image[0..-5]]= JSON.parse(response.body)
+
+      initial_res = collect[crop_image[0..-5]].dig("regions",0,"lines",0,"words",0,"text").to_s
+      mid_res = collect[crop_image[0..-5]].dig("regions",0,"lines",0,"words",1,"text").to_s
+      final_res = collect[crop_image[0..-5]].dig("regions",0,"lines",0,"words",2,"text").to_s
+
+      collect[crop_image[0..-5]] = initial_res + ' ' + mid_res + ' ' + final_res
+
+      puts collect[crop_image[0..-5]]
     end
-
-    json_file.puts(response.body)
-
+    data.write(collect.values)
   end
-   json_file.close
+  collect
 end
+
+
+
+
+
 
 
 
