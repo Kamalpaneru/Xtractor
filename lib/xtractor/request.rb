786548b27811a967cd4d8e6ac836692fb2af4fe7
kamalpaneru <kamalpaneru.15@gmail.com>
2017-10-08 15:57:57 +0545

    Issue #3

--- lib/xtractor/request.rb
+++ lib/xtractor/request.rb
@@ -1,48 +1,64 @@
 require 'net/http'
 require 'json'
 
-def request_API
-  uri = URI('https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/ocr')
-  uri.query = URI.encode_www_form({
+class Azure_API
 
-    'language' => 'en',
-    'detectOrientation ' => 'true'
+  extend Xtractor
 
-  })
+  def request_API
+    uri = URI('https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/ocr')
+    uri.query = URI.encode_www_form({
 
-  request = Net::HTTP::Post.new(uri.request_uri)
+      'language' => 'en',
+      'detectOrientation ' => 'true'
 
-  request['Content-Type'] = 'application/octet-stream'
+    })
 
-  request['Ocp-Apim-Subscription-Key'] = "4ba1e7dfca694ca7bfce73b99988395c"
+    request = Net::HTTP::Post.new(uri.request_uri)
 
-  collect = Hash.new
+    request['Content-Type'] = 'application/octet-stream'
 
-  File.open('final.json', "w") do |data|
-    all_files = Dir.glob("cell-files/*.{jpg,png,gif}").sort
+    request['Ocp-Apim-Subscription-Key'] = "4ba1e7dfca694ca7bfce73b99988395c"
 
-    all_files.each do |crop_image|
-     request.body = File.binread("#{crop_image}")
+    collect = Hash.new
 
-      response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
-       http.request(request)
+      all_files = Dir.glob("cell-files/*.{jpg,png,gif}").sort
+
+      all_files.each do |crop_image|
+        request.body = File.binread("#{crop_image}")
+
+        response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+        http.request(request)
+        end
+
+        collect[crop_image[0..-5]]= JSON.parse(response.body)
+
+        initial_res = collect[crop_image[0..-5]].dig("regions",0,"lines",0,"words",0,"text").to_s
+        mid_res = collect[crop_image[0..-5]].dig("regions",0,"lines",0,"words",1,"text").to_s
+        final_res = collect[crop_image[0..-5]].dig("regions",0,"lines",0,"words",2,"text").to_s
+
+        collect[crop_image[0..-5]] = initial_res + ' ' + mid_res + ' ' + final_res
+
+        puts collect[crop_image[0..-5]]
+
+        File.open("cell-files/#{crop_image[11..-5]}.txt", "w") do |data|
+            data.write(collect[crop_image[0..-5]])
+            end
       end
-
-      collect[crop_image[0..-5]]= JSON.parse(response.body)
-
-      initial_res = collect[crop_image[0..-5]].dig("regions",0,"lines",0,"words",0,"text").to_s
-      mid_res = collect[crop_image[0..-5]].dig("regions",0,"lines",0,"words",1,"text").to_s
-      final_res = collect[crop_image[0..-5]].dig("regions",0,"lines",0,"words",2,"text").to_s
-
-      collect[crop_image[0..-5]] = initial_res + ' ' + mid_res + ' ' + final_res
-
-      puts collect[crop_image[0..-5]]
     end
-    data.write(collect.values)
-  end
-  collect
-end
 
+def  out_final(img)
+      text_row = []
+      output_file = File.open('table.tsv', 'w')
+      rows_filter(img)[0..-2].each_with_index do |row, i|
+          columns_filter(img)[0..-2].each_with_index do |column, j|
+              text_row << File.open("cell-files/#{j}x#{i}.txt", 'r').readlines.map{|line| line.strip}.join(" ")
+          end
+          output_file.puts( text_row.join("\t"))
+      end
+      output_file.close
+    end
+end
 
 
 
